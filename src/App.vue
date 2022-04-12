b2fcb49b8fbb4d1e8a57dbcd565b7ceb604e6828
andrea1632 <andrea.bellucci10@libero.it>
2022-04-12 15:08:06 +0200

    added HeaderComp

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <HeaderComp></HeaderComp>
+  </div>
+</template>
+
+<script>
+
+import HeaderComp from './components/HeaderComp.vue'
+export default {
+  name: 'App',
+  components: {
+    HeaderComp,
+  }
+}
+</script>
+
+<style lang="scss">
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  border-top: 30px solid black;
+  border-bottom: 30px solid black;
+  padding-top: 10px;
+}
+</style>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,99 @@
+<template>
+    <div class="container">
+        <div>
+            <img src="img/dc-logo.png" alt="">
+        </div>
+        <ul>
+            <li v-for="(elm, index) in headerList" :key="index">
+                <a :href="elm.ref">{{elm.name}}</a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+
+
+<script> 
+
+export default{
+    name: 'HeaderComp',
+    data: function () {
+  return {
+    headerList: [
+        {
+            'name': 'character',
+            'ref': '/character',
+        },
+               {
+            'name': 'comics',
+            'ref': '/comics',
+        },
+               {
+            'name': 'movies',
+            'ref': '/movies',
+        },
+               {
+            'name': 'tv',
+            'ref': '/tv',
+        },
+               {
+            'name': 'games',
+            'ref': '/games',
+        },
+               {
+            'name': 'collectibles',
+            'ref': '/collectibles',
+        },
+               {
+            'name': 'videos',
+            'ref': '/videos',
+        },
+               {
+            'name': 'fans',
+            'ref': '/fans',
+        },
+               {
+            'name': 'news',
+            'ref': '/news',
+        },
+                {
+            'name': 'shop',
+            'ref': '/shop',
+        },
+    ]
+  }
+}
+}
+
+
+
+</script>
+
+<style scooped>
+    li{
+        list-style: none;
+        margin: 0 10px;
+        
+    }
+    .container{
+        display: flex;
+        width: 80%;
+        margin: 0 auto;
+        justify-content: space-around;
+    }
+    a{
+        text-transform: uppercase;
+        text-decoration: none;
+        color: black;
+        font-weight: 600;
+        padding-bottom: 30px;
+    }
+    ul{
+        display: flex;
+        align-items: center;
+    }
+    a:hover{
+        border-bottom: 3px solid #0282F9;
+        color: #0282F9;
+    }
+</style>
