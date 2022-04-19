f7585711ff68b6d80220f5fe1d646e7f02ad38e8
andrea1632 <andrea.bellucci10@libero.it>
2022-04-19 17:06:55 +0200

    added content components

---
+++ src/components/CardListComp.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="card">
+        <div class="imgContainer">
+            <img :src="details.thumb" alt="">
+        </div>
+        <span>
+            {{details.series}}
+        </span>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'CardListComp', 
+    props: ['details']
+}
+</script>
+
+<style scoped>
+    span{
+        color :white;
+        text-transform: uppercase;
+        margin: 15px 0;
+        display: inline-block;
+        max-width: 250px;
+    }
+    img{
+        width: 100%;
+        height: 100%;
+    }
+    .imgContainer{
+    height: 250px;
+    width: 220px;
+    }
+    .card{
+    margin-bottom: 50px;
+    }
+</style>
